db89bd36271cd43ee5fccd4157bf5b72fe0d7ede
Sanket Chirame <sanketchirame12@gmail.com>
2016-12-04 14:58:26 +0530

    axis problem fixed

--- propg.m
+++ propg.m
@@ -2,12 +2,15 @@ function xdot = propg(t,x)
 q = x(1:4);
 w = x(5:7);
 % user input
-B_orb = [0 0.3 1]'; %magnetic filed vector assumed get it from user in body frame 
+lat = 19.08;
+lon = 72.87;
+[XYZ] = igrfmagm(550000, lat, lon, decyear(2016,1,1),12);
+B_orb = 1e-9*(XYZ)'; %magnetic filed vector assumed get it from user in body frame 
                         % to do get this value using lat-long
-I = 5e-3;   % current in amps 
+I = 6e-3;   % current in amps 
 
 MI = 0.15; % moment of inertia 1/6M*L*L with M=10kg and L = 0.3
-N = 100;   % number of turns in each torquer N = 100;
+N = 40;   % number of turns in each torquer N = 100;
 W = [ 0 w(3) -w(2) w(1);
       -w(3) 0 w(1) w(2);
       w(2) -w(1) 0 w(3);
@@ -21,12 +24,12 @@ TBI = [ q0^2 + q1^2 - q2^2 - q3^2,         2*q0*q3 + 2*q1*q2,         2*q1*q3 - 
 
 B_b = TBI*B_orb; % convert orbit frame magnetic vector in body frame
 
-tau_b = N*I*[ B_b(3)-B_b(2); B_b(1)-B_b(3); B_b(2)-B_b(1) ]; %calculate tau in orbit frame for cubic geomtry
+tau_b = N*I*0.09*[ B_b(3)-B_b(2); B_b(1)-B_b(3); B_b(2)-B_b(1) ]; %calculate tau in orbit frame for cubic geomtry
 % torquers are palced such that outwrd normals are in positive axes direction
 
-wdot = tau_b/MI; 
+wdot = tau_b/MI ;
 qdot = 0.5*W*q;
-
+tau_b'
 xdot = [qdot;wdot];
 
 end
--- torque_n.m
+++ torque_n.m
@@ -4,22 +4,23 @@
 %
 clear
 clc
-x_init = [0;0;0;1;0;0;0]; %If you change initial angles in x_init initial co-ordinates p1,p2.. has to be changed
-tspan = [0 5]; %for 5 seconds
-
-p1 = [-1 -1 -1];%Vertices of cube initial 
-p2 = [-1 -1 1];
-p3 = [-1 1 1];
-p4 = [-1 1 -1];
-p5 = [1 -1 -1];
-p6 = [1 -1 1];
-p7 = [1 1 1];
-p8 = [1 1 -1];
+x_init = [0;0;0;1;-0.3;0.2; 0.3]; %If you change initial angles in x_init initial co-ordinates p1,p2.. has to be changed
+tspan = [0 30]; %for 5 seconds
+a = 0.15;
+p1 = [-a -a -a];%Vertices of cube initial 
+p2 = [-a -a a];
+p3 = [-a a a];
+p4 = [-a a -a];
+p5 = [a -a -a];
+p6 = [a -a a];
+p7 = [a a a];
+p8 = [a a -a];
 
 P = [p1' p2' p3' p4' p5' p6' p7' p8'];
 [t,x] = ode45(@propg, tspan, x_init);
 %%
 for j = 1:length(t)   % generate the cube at each step for particular bidy frame using quaternions at that instant
+    j;
     state = x(j,:);
     q = state(1:4);
     q0 = q(4); q1 = q(1); q2 = q(2); q3 = q(3);
@@ -32,13 +33,16 @@ for j = 1:length(t)   % generate the cube at each step for particular bidy frame
        new_plot = [P_n(:,1) P_n(:,2) P_n(:,6) P_n(:,5) P_n(:,1) P_n(:,4) P_n(:,8) P_n(:,7) ...
             P_n(:,3) P_n(:,4) P_n(:,8) P_n(:,5) P_n(:,6) P_n(:,7) P_n(:,3) P_n(:,2)]';
        
-       plot3(new_plot(:,1),new_plot(:,2),new_plot(:,3));  % plot cube axes=('..') can be 
+       plot3(new_plot(:,1),new_plot(:,2),new_plot(:,3),'Marker','o');  % plot cube axes=('..') can be 
                                                           % changed to get
                                                           % better view
-                                                          % look " help axes"
+                                                         % look " help axes"
        grid on
        xlabel('x'); ylabel('y'); zlabel('z');
-
+       axis('equal')
+       
+       axis([-0.3 0.3 -0.3 0.3 -0.3 0.3]);
        drawnow; %draw the figure
        pause(tspan(2)/length(t)); %pause to get real time fill
+       %pause(0.05)
 end
